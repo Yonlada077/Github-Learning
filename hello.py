572a933bf914fe93ff48917874b31b88f841881f
Punmanat <60070053@kmitl.ac.th>
2017-10-21 14:22:44 +0700

    Add print

--- hello.py
+++ hello.py
@@ -2,4 +2,11 @@ def main(num):
 	for _ in range(10):
 		print("EEEE")
 		print("Hello")
+		print('KUYYAIMARK')
+		print('KUYYAIMARK')
+		print('KUYYAIMARK')
+		print('KUYYAIMARK')
+		print('KUYYAIMARK')
+		print('KUYYAIMARK')
+		print('KUYYAIMARK')
 main(int(input()))
